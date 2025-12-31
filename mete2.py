dbefbf770afd1b8454aafa8fe78c7bf839a07cca
metehanzincirkiran <zincirkiranmetehan@gmail.com>
2025-12-31 15:53:42 +0300

    Add files via upload
    Dünki kodları kısa bir tekrar amaçlı

---
+++ mete2.py
@@ -0,0 +1,11 @@
+benim_maaşim = 10000
+faturalar = 5000
+market_alişverişi =2000
+
+toplam_gider = faturalar + market_alişverişi
+bana_kalan_para = benim_maaşim - toplam_gider
+
+print("\n\t-HAFTALİK MASRAFLAR")
+print("-" * 30)
+print(f"\n\t-Benim maaşim: {benim_maaşim}\n\t-Faturalar: {faturalar}\n\t-Market Alişverişi: {market_alişverişi}")
+print(f"\n\t-Toplam Gider: {toplam_gider}\n\t-Bana Kalan Para: {bana_kalan_para}")
