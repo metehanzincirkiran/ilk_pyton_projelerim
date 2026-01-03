32c96bdd9904faa2313972d44718d890731d4450
metehanzincirkiran <zincirkiranmetehan@gmail.com>
2026-01-03 13:28:42 +0300

    Add files via upload
    bugün bu çalışmayı bitirdim. 1 saat mola ve ardından input öğrenmeye geçeceğim.

---
+++ deneme.py
@@ -0,0 +1,22 @@
+mesaj = 5 
+mesaj2 = 4
+toplam = mesaj + mesaj2
+print(f"İşlemin Sonucu:{toplam}")
+
+#ilk kodumuz burada bitti bir sonrakine geçiyoruz.
+
+Telefon_Bir = 200
+Telefon_İki = 300
+Telefon_Üç = 350 
+
+İlk_Hafta_Okunacak_Sayfa_Sayisi = Telefon_Bir
+İkinci_Hafta_Okunacak_Sayda_Sayisi = Telefon_İki
+Üçüncü_Hafta_Okunacak_Sayfa_Sayisi = Telefon_Üç
+İki_Hafta_Boyunca_Okunan_Toplam_Sayfa = Telefon_Bir + Telefon_İki
+Üç_Hafta_Boyunca_Okunan_Toplam_Sayfa_Sayisi = Telefon_Bir + Telefon_İki + Telefon_Üç
+
+print("\n\t-ÜÇ HAFTA BOYUNCA OKUNAN SAYFA SAYİSİ")
+print("-" * 65)
+print(f"\n\t-İlk Hafta Okunan Toplam Sayfa: {Telefon_Bir}\n\t-İkinci Hafta Okunan Toplam Sayfa Sayisi: {Telefon_İki}\n\t-Üçüncü Hafta Okunan Toplam Sayfa Sayisi: {Telefon_Üç}")
+print("-" * 65)
+print(f"\n\t-İki Hafta Boyunca Okunan Toplam Sayfa Sayisi: {İki_Hafta_Boyunca_Okunan_Toplam_Sayfa}\n\t-Üç Hafta Boyunca Okunan Toplam Sayfa Sayisi: {Üç_Hafta_Boyunca_Okunan_Toplam_Sayfa_Sayisi}")
